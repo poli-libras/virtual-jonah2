87a178cbc058751cf7263b1d57ea659a14970cc4
Eduardo Kuroda <eduardohideo@gmail.com>
2014-02-14 00:17:10 -0200

    fixed loader tests

--- src/loader_json.js
+++ src/loader_json.js
@@ -1,10 +1,12 @@
-function load_model_json(model_file){
+function load_model_json(model_file,callback){
     var loader = new THREE.JSONLoader();
-    return loader.load(model_file,config_human);
+    loader.load(model_file,config_human);
+    
+    function config_human(geometry){
+	var human = new THREE.SkinnedMesh(geometry, new THREE.MeshLambertMaterial());
+	human.material.skinning = true;
+	callback(human);
+    }
+
 }
 
-function config_human(geometry){
-    human = new THREE.SkinnedMesh(geometry, new THREE.MeshLambertMaterial());
-    human.material.skinning = true;
-    return human;
-}
--- test/core/loader_tests.js
+++ test/core/loader_tests.js
@@ -1,5 +1,8 @@
-test("should_load_a_model",function(){
-    var h = load_model_json("../resources/model/human.js");
-    console.log(h);
+asyncTest("should_load_a_model",function(){
+    expect(1);
+    load_model_json("../resources/model/human.js",check_model);
+    function check_model(human){
+	equal(human.id,1,"model loaded");
+	start();
     }
-);
+});
