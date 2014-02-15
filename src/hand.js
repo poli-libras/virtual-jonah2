ca90538e1f0a06f8039f0ddcc8fde097608064b4
Eduardo Kuroda <eduardohideo@gmail.com>
2014-02-15 00:18:14 -0200

    updated hand class

--- src/hand.js
+++ src/hand.js
@@ -1,16 +1,14 @@
-function Hand(shape,loc,arm_bone,hand_bone){
+function Hand(shape,loc,arm_bones,hand_bone){
     this.shape = shape;
     this.yaw = 0;
     this.pitch = 0;
     this.roll = 0;
     this.loc = loc;
     this.arm_bones = arm_bones;
-    this.hand_bones = hand_bones;
 }
 
 Hand.prototype.update_pose = function(){
     this.arm_bones.update_pose(yaw,pitch,roll,loc);
-    this.hand_bones.update_pose(shape);
 };
 
 function Arm_bones(up_arm,lo_arm,wrist){
@@ -19,17 +17,27 @@ function Arm_bones(up_arm,lo_arm,wrist){
     this.wrist = wrist;
 }
 
-Arm_bones.prototype.update_pose = function(yaw,pitch,roll,loc){
-    up_arm.rot._quartenion.x = 1;
-    up_arm.rot._quartenion.y = 0;
-    up_arm.rot._quartenion.z = 0;
+Arm_bones.prototype.update_wrist = function(yaw,pitch,roll){
+    this.wrist.rot.x = yaw;
+    this.wrist.rot.y = pitch;
+    this.wrist.rot.z = roll;
 };
 
-function Hand_bones(finger1,finger2,finger3,finger4,finger5){
-    this.finger1 = finger1;
-    this.finger2 = finger2;
-    this.finger3 = finger3;
-    this.finger4 = finger4;
-    this.finger5 = finger5;
-}
+
+Arm_bones.prototype.update_loc = function(loc){
+    if (loc == "ORELHA"){
+	var up_arm_rot = {x:1,y:0,z:0};
+	var lo_arm_rot = {x:0,y:1,z:0};
+	this.update_arm(up_arm_rot,lo_arm_rot);
+    }
+};
+
+Arm_bones.prototype.update_arm = function(up_arm_rot,lo_arm_rot){
+    this.up_arm.rot.x = up_arm_rot.x;
+    this.up_arm.rot.y = up_arm_rot.y;
+    this.up_arm.rot.z = up_arm_rot.z;
+    this.lo_arm.rot.x = lo_arm_rot.x;
+    this.lo_arm.rot.y = lo_arm_rot.y;
+    this.lo_arm.rot.z = lo_arm_rot.z;
+};
 
--- test/core/hand_tests.js
+++ test/core/hand_tests.js
@@ -1,3 +1,21 @@
-test("hello test", function(){
-    ok( 1 == "1", "Passed!");
+var up_arm,lo_arm,wrist;
+
+module("module hand",{
+setup: function() {
+    up_arm = {rot:{x:0,y:0,z:0}};
+    lo_arm = {rot:{x:0,y:0,z:0}};
+    wrist = {rot:{x:0,y:0,z:0}};
+}});
+
+test("should_update_wrist", function(){
+    var arm_bones = new Arm_bones(up_arm,lo_arm,wrist);
+    arm_bones.update_wrist(90,90,90);
+    deepEqual(wrist.rot,{x:90,y:90,z:90}); 
+});
+
+test("should_update_location", function(){
+    var arm_bones = new Arm_bones(up_arm,lo_arm,wrist);
+    arm_bones.update_loc("ORELHA");
+    deepEqual(up_arm.rot,{x:1,y:0,z:0}); 
+    deepEqual(lo_arm.rot,{x:0,y:1,z:0}); 
 });
