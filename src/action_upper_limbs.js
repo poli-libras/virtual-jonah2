6a12ced9f488f85d9e111382be44f3bc78f3fc91
fabiopicchi <fabio.exe@gmail.com>
2014-10-15 13:07:30 -0300

    bones now accessed by their names

--- src/action_upper_limbs.js
+++ src/action_upper_limbs.js
@@ -10,10 +10,10 @@ this.vj2 = this.vj2||{};
     var p = Action_upper_limbs.prototype;
 
     p.create_upper_limbs = function(){
-        var arm_bones_left = this.create_arm_bones(26,27,28,"LEFT");
-        var arm_bones_right = this.create_arm_bones(52,53,54,"RIGHT"); 
-        var hand_left = this.create_hand_left();
-        var hand_right = this.create_hand_right();
+        var arm_bones_left = this.create_arm_bones("LEFT");
+        var arm_bones_right = this.create_arm_bones("RIGHT"); 
+        var hand_left = this.create_hand("LEFT");
+        var hand_right = this.create_hand("RIGHT");
         this.upper_limbs_left= new vj2.Upper_limbs(hand_left,arm_bones_left,"LEFT");
         this.upper_limbs_right= new vj2.Upper_limbs(hand_right,arm_bones_right,"RIGHT");
     };
@@ -46,36 +46,37 @@ this.vj2 = this.vj2||{};
         this.upper_limbs_left.animation(dt);
     };
 
-    p.create_arm_bones = function(up_arm_index,lo_arm_index,wrist_index,direction){
-        var up_arm_left = this.human_model.skeleton.bones[up_arm_index].quaternion;
-        var lo_arm_left = this.human_model.skeleton.bones[lo_arm_index].quaternion;
-        var wrist_left = this.human_model.skeleton.bones[wrist_index].quaternion;
+    p.get_bone = function(bone_name){
+        var bones = this.human_model.skeleton.bones;
+        for(var i = 0; i < bones.length; i++)
+            if(bones[i].name == bone_name)
+                return bones[i];
+    };
+
+    p.create_arm_bones = function(direction){
+        var up_arm_left = this.get_bone("UpArm_" + direction.charAt(0)).quaternion;
+        var lo_arm_left = this.get_bone("LoArm_" + direction.charAt(0)).quaternion;
+        var wrist_left = this.get_bone("Hand_" + direction.charAt(0)).quaternion;
+
         return new vj2.Arm_bones(up_arm_left,lo_arm_left,wrist_left,direction);
     };
 
-    p.create_hand_left = function(){
-        var thumb = this.create_finger(48,49,50,"LEFT");
-        var index = this.create_finger(31,32,33,"LEFT");
-        var middle = this.create_finger(35,36,37,"LEFT");
-        var ring = this.create_finger(40,41,42,"LEFT");
-        var pinky = this.create_finger(44,45,46,"LEFT");
-        return new vj2.Hand(thumb,index,middle,ring,pinky,"LEFT");
+    p.create_hand = function(direction){
+        var ar_fingers = [];
+
+        for(var i = 1; i <=5; i++)
+            ar_fingers.push(this.create_finger(i,direction));
+
+        return new vj2.Hand(ar_fingers[0],ar_fingers[1],ar_fingers[2],ar_fingers[3],ar_fingers[4],direction);
     };
 
-    p.create_hand_right = function(){
-        var thumb = this.create_finger(74,75,76,"RIGHT");
-        var index = this.create_finger(57,58,59,"RIGHT");
-        var middle = this.create_finger(61,62,63,"RIGHT");
-        var ring = this.create_finger(66,67,68,"RIGHT");
-        var pinky = this.create_finger(70,71,72,"RIGHT");
-        return new vj2.Hand(thumb,index,middle,ring,pinky,"RIGHT");
-    };
+    p.create_finger = function(finger_index, direction){
+        var ar_bones = [];
 
-    p.create_finger = function(index_phalange1,index_phalange2,index_phalange3,direction){
-        var bone1 = this.human_model.skeleton.bones[index_phalange1].quaternion;
-        var bone2 = this.human_model.skeleton.bones[index_phalange2].quaternion;
-        var bone3 = this.human_model.skeleton.bones[index_phalange3].quaternion;
-        return new vj2.Finger(bone1,bone2,bone3,direction);
+        for(var i = 1;i <=3; i++)
+            ar_bones.push(this.get_bone("Finger-" + finger_index + "-" + i + "_" + direction.charAt(0)).quaternion);
+
+        return new vj2.Finger(ar_bones[0],ar_bones[1],ar_bones[2],direction);
     };
 
     vj2.Action_upper_limbs = Action_upper_limbs;
